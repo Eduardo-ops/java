d8750c65add0602ae310647c13549ec7564c4175
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2023-01-03 14:53:27 -0300

    Testes automarizados e exercicios do uri de 1008 a 1012

---
+++ uri/src/exercicesuri1009/Main.java
@@ -0,0 +1,33 @@
+package exercicesuri1009;
+
+import java.util.Scanner;
+
+/**
+ * Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total
+ * de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor
+ * ganha 15% de comissão sobre suas vendas efetuadas, informar o total a receber
+ * no final do mês, com duas casas decimais.
+ * 
+ * ENTRADA
+ * O arquivo de entrada contém um texto (primeiro nome do vendedor) e 2 valores
+ * de dupla precisão (double) com duas casas decimais, representando o salário
+ * fixo do vendedor e montante total das vendas efetuadas por este vendedor,
+ * respectivamente.
+ * 
+ * SAÍDA
+ * Imprima o total que o funcionário deverá receber, conforme exemplo fornecido.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String sellerName = sc.next();
+		double salary = sc.nextDouble();
+		double totalSales = sc.nextDouble();
+
+		salary = salary + (totalSales * 0.15);
+		System.out.println(String.format("TOTAL = R$ %.2f", salary));
+	}
+
+}
