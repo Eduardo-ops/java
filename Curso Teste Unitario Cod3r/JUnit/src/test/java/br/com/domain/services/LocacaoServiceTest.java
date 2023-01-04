5cf3bd8be325b6edb1f60dbd147597182b4ff26c
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2023-01-04 15:04:59 -0300

    AssertThat e Fluent Interface

--- Curso Teste Unitario Cod3r/JUnit/src/test/java/br/com/domain/services/LocacaoServiceTest.java
+++ Curso Teste Unitario Cod3r/JUnit/src/test/java/br/com/domain/services/LocacaoServiceTest.java
@@ -1,7 +1,17 @@
 package br.com.domain.services;
 
+import static br.com.domain.utils.DataUtils.isMesmaData;
+import static br.com.domain.utils.DataUtils.obterDataComDiferencaDias;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Date;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.core.Is;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -27,9 +37,15 @@ public class LocacaoServiceTest {
 		// Acao
 		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
 
-		// Validacao
+		// Validacao modo 1
 		Assert.assertEquals(6.50, locacao.getValor(), 0.01);
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+
+		// Validacao modo 2 - Fluent Interface
+		assertThat(locacao.getValor(), is(equalTo(6.50)));
+		assertThat(locacao.getValor(), is(not(6.0)));
+		assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
 	}
 }
