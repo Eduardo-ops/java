d8854758374a048f240d229e76ca491c09d1bfad
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2023-01-07 09:32:33 -0300

    Uri 1015

---
+++ uri/src/exercicesuri1015/Main.java
@@ -0,0 +1,36 @@
+package exercicesuri1015;
+
+import java.util.Scanner;
+
+/**
+ * Leia os quatro valores correspondentes aos eixos x e y de dois pontos
+ * quaisquer no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles,
+ * mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+ * 
+ * Distancia = raiz((x2 - x1)^2 + (y2 - y1))^2
+ * 
+ * ENTRADA 
+ * O arquivo de entrada contém duas linhas de dados. A primeira linha
+ * contém dois valores de ponto flutuante: x1 y1 e a segunda linha contém dois
+ * valores de ponto flutuante x2 y2.
+ * 
+ * SAÍDA 
+ * Calcule e imprima o valor da distância segundo a fórmula fornecida, com
+ * 4 casas após o ponto decimal.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		double x1 = sc.nextDouble();
+		double y1 = sc.nextDouble();
+		double x2 = sc.nextDouble();
+		double y2 = sc.nextDouble();
+		double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+		System.out.println(String.format("%.4f", distance));
+
+	}
+
+}
