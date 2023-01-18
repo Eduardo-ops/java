98c1314e9eceede45c1a811c0b446c2fa6d4870c
Eduardo Isidoro Goncalves <eduardof013@hotmail.com>
2023-01-18 12:16:03 -0300

    TDD basico

---
+++ Curso Teste Unitario Cod3r/JUnit/src/main/java/br/com/domain/services/CalculadoraService.java
@@ -0,0 +1,27 @@
+package br.com.domain.services;
+
+import br.com.domain.exceptions.NaoPodeDividirPorZeroException;
+
+public class CalculadoraService {
+
+	public int somar(int a, int b) {
+		return a + b;
+	}
+
+	public int subtrair(int a, int b) {
+		return a - b;
+	}
+
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+
+	public int dividir(int a, int b) throws NaoPodeDividirPorZeroException {
+		if (b == 0) {
+			throw new NaoPodeDividirPorZeroException();
+		}
+
+		return a / b;
+	}
+
+}
--- Curso Teste Unitario Cod3r/JUnit/src/test/java/br/com/domain/services/CalculadoraTest.java
+++ Curso Teste Unitario Cod3r/JUnit/src/test/java/br/com/domain/services/CalculadoraTest.java
@@ -1,5 +1,68 @@
 package br.com.domain.services;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.domain.exceptions.NaoPodeDividirPorZeroException;
+import junit.framework.Assert;
+
 public class CalculadoraTest {
-	
+
+	private static CalculadoraService calculadora;
+
+	@Before
+	public void setup() {
+		calculadora = new CalculadoraService();
+	}
+
+	@Test
+	public void testeSomarDoisValores() {
+		// Cenario
+		int a = 5;
+		int b = 10;
+
+		// Acao
+		int resultado = calculadora.somar(a, b);
+
+		// Validacao
+		Assert.assertEquals(15, resultado);
+	}
+
+	@Test
+	public void testeSubtrairDoisValores() {
+		int a = 5;
+		int b = 10;
+
+		int resultado = calculadora.subtrair(a, b);
+
+		Assert.assertEquals(-5, resultado);
+	}
+
+	@Test
+	public void testeMultiplicacaoDoisValores() {
+		int a = 5;
+		int b = 10;
+
+		int resultado = calculadora.multiplicar(a, b);
+
+		Assert.assertEquals(50, resultado);
+	}
+
+	@Test
+	public void testeDividirDoisValores() throws NaoPodeDividirPorZeroException {
+		int a = 10;
+		int b = 5;
+
+		int resultado = calculadora.dividir(a, b);
+
+		Assert.assertEquals(2, resultado);
+	}
+
+	@Test(expected = NaoPodeDividirPorZeroException.class)
+	public void lancarExcecaoAoDividirPorZero() throws NaoPodeDividirPorZeroException {
+		int a = 10;
+		int b = 0;
+
+		calculadora.dividir(a, b);
+	}
 }
