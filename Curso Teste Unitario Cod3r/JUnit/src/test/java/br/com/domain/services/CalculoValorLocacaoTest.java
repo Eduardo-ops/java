c36ce785e4abab14c2e78044940d37df4a9bb9cf
Eduardo Isidoro Goncalves <eduardof013@hotmail.com>
2023-01-24 16:49:29 -0300

    Testes parametrizaveis

---
+++ Curso Teste Unitario Cod3r/JUnit/src/test/java/br/com/domain/services/CalculoValorLocacaoTest.java
@@ -0,0 +1,74 @@
+package br.com.domain.services;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import br.com.domain.entities.Filme;
+import br.com.domain.entities.Locacao;
+import br.com.domain.entities.Usuario;
+import br.com.domain.exceptions.FilmeSemEstoqueException;
+import br.com.domain.exceptions.LocadoraException;
+
+@RunWith(Parameterized.class)
+public class CalculoValorLocacaoTest {
+
+	private LocacaoService locacaoService;
+
+	@Parameter
+	public List<Filme> listFilmes;
+
+	@Parameter(value = 1)
+	public Double valorLocacao;
+
+	@Parameter(value = 2)
+	public String cenario;
+
+	@Before
+	public void setup() {
+		locacaoService = new LocacaoService();
+	}
+
+	private static Filme filme1 = new Filme("filme1", 5, 4.0);
+	private static Filme filme2 = new Filme("filme2", 5, 4.0);
+	private static Filme filme3 = new Filme("filme3", 5, 4.0);
+	private static Filme filme4 = new Filme("filme4", 5, 4.0);
+	private static Filme filme5 = new Filme("filme5", 5, 4.0);
+	private static Filme filme6 = new Filme("filme6", 5, 4.0);
+
+	@Parameters(name = "{2}")
+	public static Collection<Object[]> getParametros() {
+		return Arrays.asList(new Object[][] { { Arrays.asList(filme1, filme2, filme3), 11.00, "3 Filmes 25%" },
+				{ Arrays.asList(filme1, filme2, filme3, filme4), 12.25, "4 Filmes 50%" },
+				{ Arrays.asList(filme1, filme2, filme3, filme4, filme5), 11.6875, "5 Filmes 75%" },
+				{ Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6), 10.015625, "6 Filmes 100%" }, });
+	}
+
+	/**
+	 * Teste que valida se o valor total a pagar está sendo calculado com o desconto
+	 * de acordo com os parametros
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testePagarFimesComDescontos() throws FilmeSemEstoqueException, LocadoraException {
+		Usuario usuario = new Usuario("Isabelle Fernandes de Campos");
+
+		Locacao locacao = locacaoService.alugarFilme(usuario, listFilmes);
+
+		assertThat(locacao.getValor(), is(valorLocacao));
+	}
+
+}
--- Curso Teste Unitario Cod3r/JUnit/src/test/java/br/com/domain/services/LocacaoServiceTest.java
+++ Curso Teste Unitario Cod3r/JUnit/src/test/java/br/com/domain/services/LocacaoServiceTest.java
@@ -57,170 +57,170 @@ public class LocacaoServiceTest {
 	/**
 	 * Teste com utiliza�ao de ferramenta
 	 */
-	@Test
-	public void testeDeveAlugarFilme() throws Exception {
-		// Garante que vai ser executado em dias diferentes que sábado
-		Assume.assumeFalse(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
-
-		// Cenario
-		Usuario usuario = new Usuario("Eduardo Isidoro Gon�alves");
-		List<Filme> listFilmes = new ArrayList<Filme>();
-
-		listFilmes.add(new Filme("Batman Begins", 5, 6.50));
-		listFilmes.add(new Filme("Avatar 2", 10, 6.50));
-
-		// Acao
-		Locacao locacao;
-
-		try {
-			locacao = locacaoService.alugarFilme(usuario, listFilmes);
-
-			// Validacao modo 1
-			Assert.assertEquals(13, locacao.getValor(), 0.01);
-			Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-			Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-
-			// Validacao modo 2 - Fluent Interface
-			assertThat(locacao.getValor(), is(equalTo(13.0)));
-			assertThat(locacao.getValor(), is(not(6.0)));
-			assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-			assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
-		} 
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	}
-
-	/**
-	 * Teste com utiliza�ao ErrorCollector, onde nos possibilita verificar todos os
-	 * poss�veis erros.
-	 * 
-	 * Utilizando tamb�m o lan�amento de exce��o, onde faz com que o JUnit faz todo
-	 * o tratamento da exce��o
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testeDeveAlugarFilmeComErrorCollector() throws Exception {
-		Usuario usuario = new Usuario("Josimar Ribeiro Cardoso");
-		List<Filme> listFilmes = Arrays.asList(new Filme("Batman Begins", 2, 6.50));
-
-		Locacao locacao = locacaoService.alugarFilme(usuario, listFilmes);
-
-		errors.checkThat(locacao.getValor(), is(equalTo(6.50)));
-		errors.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-		errors.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
-	}
-
-	/**
-	 * Teste passando uma exception na anota��o, onde o teste vai ser validado se
-	 * realmente for lan�ado a exce��o esperada e somente deve ser usado quando
-	 * realmente tiver certeza do retorno da exce��o esperada, uma forma mais
-	 * elegante
-	 * 
-	 * JUnit faz todo tratamento de exce��o.
-	 * 
-	 * @throws Exception
-	 */
-	@Test(expected = FilmeSemEstoqueException.class)
-	public void testeNaoDeveAlugarFilmeSemEstoque() throws Exception {
-		Usuario usuario = new Usuario("Josimar Ribeiro Cardoso");
-		List<Filme> listFilmes = Arrays.asList(new Filme("Batman Begins", 0, 6.50));
-
-		locacaoService.alugarFilme(usuario, listFilmes);
-	}
-
-	/**
-	 * Mesma finalidade do m�todo testeLocacaoFilmeSemEstoque, por�m aqui tratamos a
-	 * exce��o de forma mais clara no pr�prio c�digo, uma forma mais robusta.
-	 * 
-	 * Instrutor recomenda usar essa forma, pelo fato de ser mais completa, ou seja,
-	 * robusta.
-	 * 
-	 * @throws LocadoraException
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testeNaoDeveAlugarFilmeSemEstoque2() throws Exception {
-		Usuario usuario = new Usuario("Josimar Ribeiro Cardoso");
-		List<Filme> listFilmes = Arrays.asList(new Filme("Batman Begins", 0, 6.50));
-
-		try {
-			locacaoService.alugarFilme(usuario, listFilmes);
-			Assert.fail("Deveria ter lan�ado uma exce��o");
-		} 
-		catch (Exception e) {
-			assertThat(e.getMessage(), is("Filme sem estoque"));
-		}
-	}
-
-	/**
-	 * Teste utilizando ExpectedException, onde dizemos qual exce��o � esperada a
-	 * ser lan�ada e qual o tipo de mensagem de retorno
-	 * 
-	 * JUnit faz todo tratamento de exce��o.
-	 * 
-	 * @throws FilmeSemEstoqueException
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testeNaoDeveAlugarFilmeSemEstoque3() throws Exception {
-		Usuario usuario = new Usuario("Josimar Ribeiro Cardoso");
-		List<Filme> listFilmes = Arrays.asList(new Filme("Batman Begins", 0, 6.50));
-
-		expectedException.expect(Exception.class);
-		expectedException.expectMessage("Filme sem estoque");
-
-		locacaoService.alugarFilme(usuario, listFilmes);
-	}
-
-	/**
-	 * Essa � a forma mais pratica de se usar a forma elegante, por�m, quando se
-	 * passa a pr�pria exception customizada no programa, e n�o uma simples
-	 * exception gen�rica
-	 * 
-	 * Teste passando uma exception na anota��o, onde o teste vai ser validado se
-	 * realmente for lan�ado a exce��o esperada.
-	 * 
-	 * JUnit faz todo tratamento de exce��o.
-	 * 
-	 * @throws Exception
-	 */
-	@Test(expected = FilmeSemEstoqueException.class)
-	public void testeNaoDeveAlugarFilmeSemEstoque4() throws FilmeSemEstoqueException, LocadoraException {
-		Usuario usuario = new Usuario("Josimar Ribeiro Cardoso");
-		List<Filme> listFilmes = Arrays.asList(new Filme("Batman Begins", 0, 6.50));
-
-		locacaoService.alugarFilme(usuario, listFilmes);
-	}
-
-	/**
-	 * Teste que faz a valida��o de usu�rio com base na forma robusta
-	 * 
-	 * * Instrutor recomenda usar essa forma, pelo fato de ser mais completa, ou
-	 * seja, robusta.
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testeNaoDeveAlugarFilmeSemUsuario() throws FilmeSemEstoqueException {
-		List<Filme> listFilmes = new ArrayList<Filme>();
-
-		listFilmes.add(new Filme("Batman Begins", 5, 6.50));
-		listFilmes.add(new Filme("Avatar 2", 10, 6.50));
-
-		try {
-			locacaoService.alugarFilme(null, listFilmes);
-			Assert.fail();
-		} 
-		catch (LocadoraException e) {
-			assertThat(e.getMessage(), is("Usu�rio obrigat�rio"));
-		}
-	}
+//	@Test
+//	public void testeDeveAlugarFilme() throws Exception {
+//		// Garante que vai ser executado em dias diferentes que sábado
+//		Assume.assumeFalse(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
+//
+//		// Cenario
+//		Usuario usuario = new Usuario("Eduardo Isidoro Gon�alves");
+//		List<Filme> listFilmes = new ArrayList<Filme>();
+//
+//		listFilmes.add(new Filme("Batman Begins", 5, 6.50));
+//		listFilmes.add(new Filme("Avatar 2", 10, 6.50));
+//
+//		// Acao
+//		Locacao locacao;
+//
+//		try {
+//			locacao = locacaoService.alugarFilme(usuario, listFilmes);
+//
+//			// Validacao modo 1
+//			Assert.assertEquals(13, locacao.getValor(), 0.01);
+//			Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+//			Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+//
+//			// Validacao modo 2 - Fluent Interface
+//			assertThat(locacao.getValor(), is(equalTo(13.0)));
+//			assertThat(locacao.getValor(), is(not(6.0)));
+//			assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+//			assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+//		} 
+//		catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	}
+//
+//	/**
+//	 * Teste com utiliza�ao ErrorCollector, onde nos possibilita verificar todos os
+//	 * poss�veis erros.
+//	 * 
+//	 * Utilizando tamb�m o lan�amento de exce��o, onde faz com que o JUnit faz todo
+//	 * o tratamento da exce��o
+//	 * 
+//	 * @throws Exception
+//	 */
+//	@Test
+//	public void testeDeveAlugarFilmeComErrorCollector() throws Exception {
+//		Usuario usuario = new Usuario("Josimar Ribeiro Cardoso");
+//		List<Filme> listFilmes = Arrays.asList(new Filme("Batman Begins", 2, 6.50));
+//
+//		Locacao locacao = locacaoService.alugarFilme(usuario, listFilmes);
+//
+//		errors.checkThat(locacao.getValor(), is(equalTo(6.50)));
+//		errors.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+//		errors.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+//	}
+//
+//	/**
+//	 * Teste passando uma exception na anota��o, onde o teste vai ser validado se
+//	 * realmente for lan�ado a exce��o esperada e somente deve ser usado quando
+//	 * realmente tiver certeza do retorno da exce��o esperada, uma forma mais
+//	 * elegante
+//	 * 
+//	 * JUnit faz todo tratamento de exce��o.
+//	 * 
+//	 * @throws Exception
+//	 */
+//	@Test(expected = FilmeSemEstoqueException.class)
+//	public void testeNaoDeveAlugarFilmeSemEstoque() throws Exception {
+//		Usuario usuario = new Usuario("Josimar Ribeiro Cardoso");
+//		List<Filme> listFilmes = Arrays.asList(new Filme("Batman Begins", 0, 6.50));
+//
+//		locacaoService.alugarFilme(usuario, listFilmes);
+//	}
+//
+//	/**
+//	 * Mesma finalidade do m�todo testeLocacaoFilmeSemEstoque, por�m aqui tratamos a
+//	 * exce��o de forma mais clara no pr�prio c�digo, uma forma mais robusta.
+//	 * 
+//	 * Instrutor recomenda usar essa forma, pelo fato de ser mais completa, ou seja,
+//	 * robusta.
+//	 * 
+//	 * @throws LocadoraException
+//	 * 
+//	 * @throws Exception
+//	 */
+//	@Test
+//	public void testeNaoDeveAlugarFilmeSemEstoque2() throws Exception {
+//		Usuario usuario = new Usuario("Josimar Ribeiro Cardoso");
+//		List<Filme> listFilmes = Arrays.asList(new Filme("Batman Begins", 0, 6.50));
+//
+//		try {
+//			locacaoService.alugarFilme(usuario, listFilmes);
+//			Assert.fail("Deveria ter lan�ado uma exce��o");
+//		} 
+//		catch (Exception e) {
+//			assertThat(e.getMessage(), is("Filme sem estoque"));
+//		}
+//	}
+//
+//	/**
+//	 * Teste utilizando ExpectedException, onde dizemos qual exce��o � esperada a
+//	 * ser lan�ada e qual o tipo de mensagem de retorno
+//	 * 
+//	 * JUnit faz todo tratamento de exce��o.
+//	 * 
+//	 * @throws FilmeSemEstoqueException
+//	 * 
+//	 * @throws Exception
+//	 */
+//	@Test
+//	public void testeNaoDeveAlugarFilmeSemEstoque3() throws Exception {
+//		Usuario usuario = new Usuario("Josimar Ribeiro Cardoso");
+//		List<Filme> listFilmes = Arrays.asList(new Filme("Batman Begins", 0, 6.50));
+//
+//		expectedException.expect(Exception.class);
+//		expectedException.expectMessage("Filme sem estoque");
+//
+//		locacaoService.alugarFilme(usuario, listFilmes);
+//	}
+//
+//	/**
+//	 * Essa � a forma mais pratica de se usar a forma elegante, por�m, quando se
+//	 * passa a pr�pria exception customizada no programa, e n�o uma simples
+//	 * exception gen�rica
+//	 * 
+//	 * Teste passando uma exception na anota��o, onde o teste vai ser validado se
+//	 * realmente for lan�ado a exce��o esperada.
+//	 * 
+//	 * JUnit faz todo tratamento de exce��o.
+//	 * 
+//	 * @throws Exception
+//	 */
+//	@Test(expected = FilmeSemEstoqueException.class)
+//	public void testeNaoDeveAlugarFilmeSemEstoque4() throws FilmeSemEstoqueException, LocadoraException {
+//		Usuario usuario = new Usuario("Josimar Ribeiro Cardoso");
+//		List<Filme> listFilmes = Arrays.asList(new Filme("Batman Begins", 0, 6.50));
+//
+//		locacaoService.alugarFilme(usuario, listFilmes);
+//	}
+//
+//	/**
+//	 * Teste que faz a valida��o de usu�rio com base na forma robusta
+//	 * 
+//	 * * Instrutor recomenda usar essa forma, pelo fato de ser mais completa, ou
+//	 * seja, robusta.
+//	 * 
+//	 * @throws Exception
+//	 */
+//	@Test
+//	public void testeNaoDeveAlugarFilmeSemUsuario() throws FilmeSemEstoqueException {
+//		List<Filme> listFilmes = new ArrayList<Filme>();
+//
+//		listFilmes.add(new Filme("Batman Begins", 5, 6.50));
+//		listFilmes.add(new Filme("Avatar 2", 10, 6.50));
+//
+//		try {
+//			locacaoService.alugarFilme(null, listFilmes);
+//			Assert.fail();
+//		} 
+//		catch (LocadoraException e) {
+//			assertThat(e.getMessage(), is("Usu�rio obrigat�rio"));
+//		}
+//	}
 
 	/**
 	 * Teste que faz a valida��o de filme com base na forma nova
