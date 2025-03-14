2a6b2125bd3af4ea9cf279f7abde15b30740eb45
Eduardo-ops <eduardof013@hotmail.com>
2025-03-13 22:58:51 -0300

    feat: add starting course of advanced java techniques.

---
+++ advanced_java_techniques/src/br/com/advanced/java/techniques/generics/ListNoGenerics.java
@@ -0,0 +1,7 @@
+package br.com.advanced.java.techniques.generics;
+
+public class ListNoGenerics {
+    public static void main(String[] args) {
+        System.out.println("Starting classe generics!!!");
+    }
+}
